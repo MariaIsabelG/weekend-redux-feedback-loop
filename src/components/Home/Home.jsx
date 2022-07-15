b39984a486905dff8065af58c557ecfe5e413036
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-07-15 16:02:29 -0500

    created a function for the home component with a button to start feedback

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+function Home (){
+
+    return(
+
+        <>
+        <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>Don't forget it!</h4>
+        </header>
+
+        <button>Start Feedback</button>
+
+        </>
+    )
+};
+
+export default Home;
